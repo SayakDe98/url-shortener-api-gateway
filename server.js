b4fa4684ccc2dc60993bbb3bab26f0e541069eab
Sayak De <sayakde777@gmail.com>
2026-03-29 00:02:02 +0530

    Added FRONTEND_URL from .env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app  = express();
 const PORT = process.env.PORT || 3000;
 
 // ── Middleware ────────────────────────────────────────────────────────────────
-app.use(cors({ origin: "http://localhost:5173"} ));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" } ));
 app.use(express.json());
 app.use(morgan("dev"));
 
